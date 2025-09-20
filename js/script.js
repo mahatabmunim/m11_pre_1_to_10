563ceeef63d4cedf3f8497f97d9c713e47a37dc2
Gazi Md. Mahatab Uddin Munim <128234933+mahatabmunim@users.noreply.github.com>
2025-09-20 15:25:29 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,129 @@
+document.write("<h1>Hello World Js</>")
+console.log("Hello Js console")
+
+// variable
+let a='mahatab munim'
+a='mahatab'
+let s='mahatab munim'
+document.write(a) ;
+console.log(a);
+
+//dt type
+number
+let a=2
+let b= 4
+console.log(a+b)
+
+// string 
+let str='Hello Js'
+console.log(str)
+
+// undefined
+let d;
+console.log(d)
+
+// null
+let data= null;
+console.log(data)
+
+// Symbol
+let sym = Symbol("Hello word") 
+console.log(sym)
+
+// BigInt
+let BigInt=334957348925230592345023458239085934583425234590235302953n
+console.log(BigInt)
+
+// object 
+let teacher={
+    name : "mahatab",
+    age: 25,
+    isPassed: true
+}
+console.log(teacher.name)
+console.log(teacher.age)
+
+// Array
+let name= ["mahatab","sovon","sohan"];
+console.log(name[0])
+console.log(name[0])
+
+// function 
+function great() {
+    console.log("hello Bangladesh")
+}
+great()
+
+
+//operator
+let ax=100
+let ay=2
+
+//arithmatic operator
+console.log (ax+ay)
+console.log (ax-ay)
+console.log (ax*ay)
+console.log (ax/ay)
+console.log (ax%ay)
+
+//assignment operator
+
+console.log (ax!=ay)
+console.log (ax+=ay)
+console.log (ax-=ay)
+console.log (ax*=ay)
+
+// loop (if)
+
+let age = 25
+if (age>=25){
+    console.log("you are an adult")
+}else{
+    console.log("you are a Minor")
+}
+
+let mark = 7 0
+
+if (mark >=80){
+    console.log("A+")
+}
+else if (mark >=70){
+    console.log("A")
+}
+else if (mark >=60){
+    console.log("A-")
+}
+else {
+    console.log(c)
+}
+
+    
+//switch
+let userType= "adminx"
+switch(userType){
+    case "admin":
+    console.log("admin User")
+    break;
+
+    case "teacher":
+    console.log("teacher User")
+    break;
+
+    case "student":
+    console.log("student User")
+    break;
+
+    default:
+        console.log("Unknow user type")
+}
+
+
+
+
+
+
+
+
+
+
+
